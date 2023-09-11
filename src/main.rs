018d5b4da02fc4490ac06f3022e0b00c18fcf9f7
YoungHa Kim <ytok1108@aol.com>
2023-09-11 22:10:33 +0900

    stock 230901~0911 aapl price

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use std::time::{Duration, UNIX_EPOCH};
-use time::OffsetDateTime;
+use time::{macros::datetime, OffsetDateTime};
 use tokio_test;
 use yahoo_finance_api as yahoo;
 
 fn main() {
     let provider = yahoo::YahooConnector::new();
-    // get the latest quotes in 1 minute intervals
-    let response = tokio_test::block_on(provider.get_latest_quotes("AAPL", "1d")).unwrap();
-    // extract just the latest valid quote summery
-    // including timestamp,open,close,high,low,volume
-    let quote = response.last_quote().unwrap();
-    let time: OffsetDateTime =
-        OffsetDateTime::from(UNIX_EPOCH + Duration::from_secs(quote.timestamp));
-    println!("At {} quote price of Apple was {}", time, quote.close);
+    let start = datetime!(2023-9-1 0:00:00.00 UTC);
+    let end = datetime!(2023-9-11 23:59:59.99 UTC);
+    // returns historic quotes with daily interval
+    let resp = tokio_test::block_on(provider.get_quote_history("AAPL", start, end)).unwrap();
+    let quotes = resp.quotes().unwrap();
+    println!("Apple's quotes in January: {:#?}", quotes);
 }
