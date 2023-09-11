93fc81d06315386f6d05822777cfd7178a2d05a9
YoungHa Kim <ytok1108@aol.com>
2023-09-11 22:06:44 +0900

    stock & data

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ https://github.com/xemwebe/yahoo_finance_api
 # crates.io
 
 https://crates.io/crates/yahoo_finance_api
+
+
+# Result 
+
+```
+
+At 2023-09-08 13:30:00.0 +00:00:00 quote price of Apple was 178.17999267578125  
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::time::{Duration, UNIX_EPOCH};
+use time::OffsetDateTime;
+use tokio_test;
+use yahoo_finance_api as yahoo;
+
 fn main() {
-    println!("Hello, world!");
+    let provider = yahoo::YahooConnector::new();
+    // get the latest quotes in 1 minute intervals
+    let response = tokio_test::block_on(provider.get_latest_quotes("AAPL", "1d")).unwrap();
+    // extract just the latest valid quote summery
+    // including timestamp,open,close,high,low,volume
+    let quote = response.last_quote().unwrap();
+    let time: OffsetDateTime =
+        OffsetDateTime::from(UNIX_EPOCH + Duration::from_secs(quote.timestamp));
+    println!("At {} quote price of Apple was {}", time, quote.close);
 }
