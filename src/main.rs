22a5ef12750aa1701accbaea099924b4154d074c
YoungHa Kim <ytok1108@aol.com>
2023-09-11 22:12:50 +0900

    Get the history of quotes for time range

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 use std::time::{Duration, UNIX_EPOCH};
-use time::{macros::datetime, OffsetDateTime};
 use tokio_test;
 use yahoo_finance_api as yahoo;
 
 fn main() {
     let provider = yahoo::YahooConnector::new();
-    let start = datetime!(2023-9-1 0:00:00.00 UTC);
-    let end = datetime!(2023-9-11 23:59:59.99 UTC);
-    // returns historic quotes with daily interval
-    let resp = tokio_test::block_on(provider.get_quote_history("AAPL", start, end)).unwrap();
-    let quotes = resp.quotes().unwrap();
-    println!("Apple's quotes in January: {:#?}", quotes);
+    let response = tokio_test::block_on(provider.get_quote_range("AAPL", "1d", "1mo")).unwrap();
+    let quotes = response.quotes().unwrap();
+    println!("Apple's quotes of the last month: {:#?}", quotes);
 }
